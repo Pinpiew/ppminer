e5c78812720341f378796562c28fcb1440c413df
pingping.wu <pingping.wu@bitmain.com>
2018-09-25 20:56:50 +0800

    add pool

---
+++ miningpoolhub.sh
@@ -0,0 +1,1 @@
+make; sudo  ./ppminer -a lyra2rev2 -o stratum+tcp://hub.miningpoolhub.com:20593 -u pinpie.mona_pin -p 123456 -t 1
---
+++ zpool_ca_d4.sh
@@ -0,0 +1,1 @@
+make; sudo  ./ppminer -a lyra2rev2 -o stratum+tcp://lyra2v2.mine.zpool.ca:4533 -u PSSw6DPoPdWa26skGH3fd9nCARiJvubvKR -p d=1 -t 1
